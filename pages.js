8d2b1f8ddb144947541ce096cbc66987055bd713
Sirzerch <eniquentind@gmail.com>
2020-08-25 13:04:17 +0200

    Mise en place du SPA

---
+++ pages.js
@@ -0,0 +1,16 @@
+let pages = document.querySelectorAll('.pages a')
+
+for(let i = 0; i < pages.length; i++) {
+    pages[i].addEventListener('click', function(e){
+        let div = this.parentNode.parentNode.parentNode.parentNode
+        let li = this.parentNode
+
+        if (li.classList.contains('active')) {
+            return false
+        }
+        div.querySelector('.pages .active').classList.remove('active')
+        li.classList.add('active')
+        div.querySelector('.page-content.active').classList.remove('active')
+        div.querySelector(this.getAttribute('href')).classList.add('active')
+    })
+}
