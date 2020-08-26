87344a488f079977e5df3609299783135349d6e8
Sirzerch <eniquentind@gmail.com>
2020-08-26 11:42:57 +0200

    Amélioration visuel de l'accueil

--- accueil.js
+++ accueil.js
@@ -1,18 +1,17 @@
 class Camera {
-    constructor(id, nom, prix, description, urlImg) {
-        this.id = id,
+    constructor(urlImg, nom, prix, description) {
+        this.urlImg = urlImg
         this.nom = nom,
         this.prix = prix,
-        this.description = description,
-        this.urlImg = urlImg
+        this.description = description
     }
 }
 let cameras = [];
-const firstCamera = new Camera(1, "Fujifilm X-T200", 499, "Un savoir-faire colorimétrique légendaire !", "images/vcam_1.jpg");
-const secondCamera = new Camera(2, "Sony a7S III", 1649, "Enregistrement vidéo professionnel jusqu'à 4K/120p !", "images/vcam_2.jpg");
-const thirdCamera = new Camera(3, "Nikon D6", 2999, "Jusqu'a 14 VPS !", "images/vcam_3.jpg");
-const fourthCamera = new Camera(4, "Olympus E-M10 MARK IV", 390, "Léger, mobile, facile à utiliser !", "images/vcam_4.jpg");
-const fifthCamera = new Camera(5, "Canon 77D", 899, "Léger, mobile, facile à utiliser !", "images/vcam_5.jpg");
+const firstCamera = new Camera("images/vcam_1.jpg", "Fujifilm X-T200", 499, "Un savoir-faire colorimétrique légendaire !");
+const secondCamera = new Camera("images/vcam_2.jpg", "Sony a7S III", 1649, "Enregistrement vidéo professionnel jusqu'à 4K/120p !");
+const thirdCamera = new Camera("images/vcam_3.jpg", "Nikon D6", 2999, "Jusqu'a 14 VPS !");
+const fourthCamera = new Camera("images/vcam_4.jpg", "Olympus E-M10 MARK IV", 390, "Léger, mobile, facile à utiliser !");
+const fifthCamera = new Camera("images/vcam_5.jpg", "Canon 77D", 899, "Léger, mobile, facile à utiliser !");
 
 cameras.push(firstCamera, secondCamera, thirdCamera, fourthCamera, fifthCamera);
 
@@ -21,16 +20,13 @@ let listeDesProduits = '';
 cameras.forEach(cam =>
     listeDesProduits += `
             <tr class="text-center">
-              <td class="align-middle">${cam.id}</td>
-              <td class="align-middle">${cam.nom}</td>
-              <td class="align-middle">${cam.prix}€</td>
-              <td class="align-middle">${cam.description}</td>
-              <td><img src=${cam.urlImg} class="img-fluid img-thumbnail w-25"></td>
-              <td class="align-middle"><button class="btn btn-primary">Détails</button></td>
+                <td class="w-25"><img src=${cam.urlImg} class="img-fluid img-thumbnail"></td>
+                <td class="align-middle">${cam.nom}</td>
+                <td class="align-middle">${cam.prix}€</td>
+                <td class="align-middle">${cam.description}</td>
+                <td class="align-middle"><button class="btn btn-primary mt-2 mb-2" onClick='details'>Détails</button></td>
             </tr>
             `
 )
-
-
 
 document.getElementById('listedesproduits').innerHTML = listeDesProduits;
---
+++ produits.js
@@ -0,0 +1,5 @@
+/*
+Lorsque qu'on CLICK sur le bouton détails on doit:
+récupérer cet élément
+supprimer les autres éléments
+*/
