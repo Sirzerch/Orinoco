c7809f7c2aacc0a7b6022d67c7978daae7cc1da2
Sirzerch <eniquentind@gmail.com>
2020-09-04 15:17:54 +0200

    Page d'accueil retravailler v1

--- accueil.js
+++ accueil.js
@@ -2,19 +2,69 @@ let url = "http://localhost:3000/api/cameras/"
 
 fetch(url).then(response =>
     response.json().then(data => {
-        console.log(data);
-        let listeDesProduits = '';
-        for(let cam of data) {
-            listeDesProduits += `
-                        <tr class="text-center">
-                            <td class="w-25"><img src=${cam.imageUrl} class="img-fluid img-thumbnail"></td>
-                            <td class="align-middle">${cam.name}</td>
-                            <td class="align-middle">${cam.price}€</td>
-                            <td class="align-middle">${cam.description}</td>
-                            <td class="align-middle"><a href="#produits" data-id="${cam._id}" class="btn btn-primary pages" role="button" aria-pressed="true">Détails</a></td>
-                        </tr>
-                        `
+        console.log(data)
+        for (let cam of data) {
+
+            let $ = function (selector) {
+                return document.querySelector(selector);
+            };
+
+            let tr = document.createElement('tr')
+            $('#listedesproduits').appendChild(tr)
+            tr.className = 'text-center'
+            tr.setAttribute('id', 'tr')
+
+
+            let td1 = document.createElement('td')
+            let td2 = document.createElement('td')
+            let td3 = document.createElement('td')
+            let td4 = document.createElement('td')
+            let td5 = document.createElement('td')
+            tr.append(td1, td2, td3, td4, td5)
+
+            td1.setAttribute('id', 'td' + 1)
+            td2.setAttribute('id', 'td' + 2)
+            td3.setAttribute('id', 'td' + 3)
+            td4.setAttribute('id', 'td' + 4)
+            td5.setAttribute('id', 'td' + 5)
+
+            td1.className = 'w-25'
+            td2.className = 'align-middle'
+            td3.className = 'align-middle'
+            td4.className = 'align-middle'
+            td5.className = 'align-middle'
+
+            let img = document.createElement('img')
+            td1.append(img)
+
+            img.className = 'img-fluid img-thumbnail'
+
+            let a = document.createElement('a')
+            td5.append(a)
+            a.className = 'btn btn-primary pages'
+            a.setAttribute('role', 'button')
+            a.setAttribute('aria-pressed', 'true')
+            a.innerHTML = 'Détails'
+
+            img.src = `${cam.imageUrl}`
+            
+            td2.innerHTML = `${cam.name}`
+
+            td3.innerHTML = `${cam.price}`
+
+            td4.innerHTML = `${cam.description}`
+
+            a.setAttribute('href', `${cam._id}`)
         }
-        document.getElementById('listedesproduits').innerHTML = listeDesProduits;
     })
 ).catch((err) => console.log("Erreur" + err));
+
+
+
+
+
+
+
+
+
+
