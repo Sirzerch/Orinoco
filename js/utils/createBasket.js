42cb57f4ef72432f60d5d16cfb8ecdd6f899aed7
Sirzerch <eniquentind@gmail.com>
2020-10-23 14:00:41 +0200

    Page panier : mise en place du total des produits

--- js/utils/createBasket.js
+++ js/utils/createBasket.js
@@ -2,8 +2,15 @@ export default class Panier {
 
 	constructor() {
         this.cost = 0
-        
+        this.products = []
 	}
 
+	addProductPrice(data) {
+		this.products.push(+`${data.price}`)
+		for(let product of this.products ) {
+			this.cost = this.cost += +product
+		}
 	
+		return this.cost
+	}
 }
