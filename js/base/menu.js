d27e0cb2c7b71d30061e0364f3aecd0b447814af
Sirzerch <eniquentind@gmail.com>
2020-12-03 17:52:13 +0100

    Responsive menu + sidebar

--- js/base/menu.js
+++ js/base/menu.js
@@ -1,16 +1,54 @@
-//Système d'onglets affichant le contenu de la page correspondant au "click" de l'utilisateur
-let pages = document.querySelectorAll('#pages .header__nav a')
-
-for(let page of pages) {
-    page.addEventListener('click', function(){
-        let num = this.getAttribute('data-pages')
-        
-        document.querySelector('#pages div.active').classList.remove('active')
-        document.querySelector('#page' + num).classList.add('active')
-        document.querySelector('#pages .header__nav a.active').classList.remove('active')
-        this.classList.add('active')
-    })
+let $ = function (selector) {
+    return document.querySelector(selector)
 }
+
+let contentHeaderNav = $('#js-header__nav')
+let burgerSidebarBody = $('#js-burger-sidebar-body')
+let burgerButton = $('#js-burger-button')
+let burgerOverlay = $('#js-burger-overlay')
+let activatedBurger = 'burger-activated'
+
+//Système d'onglets affichant le contenu de la page correspondant au "click" de l'utilisateur
+let navOngletsPages = function() {
+    let pages = document.querySelectorAll('#pages .header__nav a')
+
+    for(let page of pages) {
+        page.addEventListener('click', function(e){
+            let num = this.getAttribute('data-pages')
+            
+            $('#pages div.active').classList.remove('active')
+            $('#page' + num).classList.add('active')
+            $('#pages .header__nav a.active').classList.remove('active')
+            this.classList.add('active')
+            closeBurger(e)
+        })
+    }
+}
+navOngletsPages()
+
+
+//Ajoute le contenu de la barre de navigation dans la sidebar
+burgerSidebarBody.innerHTML = contentHeaderNav.innerHTML 
+
+//Rajoute la classe 'burger-activated' pour activer la sidebar
+let openBurger = function (e) {
+    e.preventDefault()
+    $('#burger-menu').classList.add(activatedBurger)
+    navOngletsPages()
+    burgerButton.addEventListener('click', closeBurger)
+}
+
+//Enlève la classe 'burger-activated' et ferme la sidebar
+let closeBurger = function(e) {
+    if($('.burger-activated')) {
+        e.preventDefault()
+        $('#burger-menu').classList.remove(activatedBurger)
+        burgerButton.removeEventListener('click', closeBurger)
+    }
+}
+
+burgerButton.addEventListener('click', openBurger)
+
 
 
 
