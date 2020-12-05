f137b6c71f46e8ed3c4bf61df8a89cb4e96aa007
Sirzerch <eniquentind@gmail.com>
2020-12-05 10:48:44 +0100

    Bouton burger en CSS

--- js/base/menu.js
+++ js/base/menu.js
@@ -8,14 +8,17 @@ let burgerButton = $('#js-burger-button')
 let burgerOverlay = $('#js-burger-overlay')
 let activatedBurger = 'burger-activated'
 
+//Ajoute le contenu de la barre de navigation dans la sidebar
+burgerSidebarBody.innerHTML = contentHeaderNav.innerHTML
+
 //Système d'onglets affichant le contenu de la page correspondant au "click" de l'utilisateur
-let navOngletsPages = function() {
+let navOngletsPages = function () {
     let pages = document.querySelectorAll('#pages .header__nav a')
 
-    for(let page of pages) {
-        page.addEventListener('click', function(e){
+    for (let page of pages) {
+        page.addEventListener('click', function (e) {
             let num = this.getAttribute('data-pages')
-            
+
             $('#pages div.active').classList.remove('active')
             $('#page' + num).classList.add('active')
             $('#pages .header__nav a.active').classList.remove('active')
@@ -27,27 +30,29 @@ let navOngletsPages = function() {
 navOngletsPages()
 
 
-//Ajoute le contenu de la barre de navigation dans la sidebar
-burgerSidebarBody.innerHTML = contentHeaderNav.innerHTML 
-
 //Rajoute la classe 'burger-activated' pour activer la sidebar
 let openBurger = function (e) {
     e.preventDefault()
+    //Gère l'animation du bouton burger
+    burgerButton.classList.toggle('actived')
     $('#burger-menu').classList.add(activatedBurger)
     navOngletsPages()
     burgerButton.addEventListener('click', closeBurger)
 }
 
 //Enlève la classe 'burger-activated' et ferme la sidebar
-let closeBurger = function(e) {
-    if($('.burger-activated')) {
+let closeBurger = function (e) {
+    if ($('.burger-activated')) {
         e.preventDefault()
+        burgerButton.classList.remove('actived')
         $('#burger-menu').classList.remove(activatedBurger)
+        navOngletsPages()
         burgerButton.removeEventListener('click', closeBurger)
     }
 }
 
 burgerButton.addEventListener('click', openBurger)
+
 
 
 
